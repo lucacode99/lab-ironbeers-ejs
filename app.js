e4f9dbaf85572712e5122ef9debb66a1d723ff37
Lucas Pfister <digitalservices@outlook.de>
2022-05-24 18:44:08 +0200

    2 tasks done, working on third

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
@@ -15,8 +16,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.get('/', (req, res) => {  res.render('index')});
+
+
+
+// WORKING UNDER HERE
+app.get('/beers', (req, res) => {  
+    punkAPI.getBeers() //promise
+    .then((beersFromApi) =>{
+        console.log(beersFromApi) 
+        //res.render('beers', {beersFromApi});
+        res.render('beers', {beersFromApi});
+    })
+    
+})
+
+
+
+
+
+// WORKING ABOVE FROM THIS
+
+app.get('/random-beer', (req, res) => {  res.render('random-beer')});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
